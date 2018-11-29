dedf5044bd9befdd6c82200db4a50d715eecc197
Edison Ferraz <edisonferraz@gmail.com>
2018-11-29 19:51:13 +0100

    Configurando Sequelize

---
+++ aulas/modulo2/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'locahost',
+  username: 'root',
+  password: '',
+  database: 'gonode_modulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
