ef8cae71201fcd98d67c60b34736dd659b221fcd
Edison Ferraz <edisonferraz@gmail.com>
2018-12-01 19:27:47 +0100

    resolucao do desafio

--- desafios/modulo2/src/app/models/Appointment.js
+++ desafios/modulo2/src/app/models/Appointment.js
@@ -1,10 +1,25 @@
+const moment = require('moment')
+
 module.exports = (sequelize, DataTypes) => {
-  const Appointment = sequelize.define('Appointment', {
-    date: DataTypes.DATE
-  })
+  const Appointment = sequelize.define(
+    'Appointment',
+    {
+      date: DataTypes.DATE
+    },
+    {
+      getterMethods: {
+        today () {
+          return moment(this.date).format('D/M/Y')
+        },
+        hour () {
+          return moment(this.date).format('HH:mm')
+        }
+      }
+    }
+  )
 
   Appointment.associate = models => {
-    Appointment.belongsTo(models.User, { foreignKey: 'user_id' })
+    Appointment.belongsTo(models.User, { as: 'user', foreignKey: 'user_id' })
     Appointment.belongsTo(models.User, { foreignKey: 'provider_id' })
   }
 
