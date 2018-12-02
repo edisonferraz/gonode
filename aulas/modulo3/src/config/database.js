494af4e2b94952d39f6be70a8163c650310aa609
Edison Ferraz <edisonferraz@gmail.com>
2018-12-02 16:00:28 +0100

    Cadastro de Usuario

--- aulas/modulo3/src/config/database.js
+++ aulas/modulo3/src/config/database.js
@@ -1,4 +1,3 @@
 module.exports = {
-  uri:
-    'mongodb+srv://edison:V22JUdtDGAuQbQg5@cluster0-v6jqg.mongodb.net/test?retryWrites=true'
+  uri: 'mongodb://edison:edison123@ds123584.mlab.com:23584/gonode_modulo3'
 }
