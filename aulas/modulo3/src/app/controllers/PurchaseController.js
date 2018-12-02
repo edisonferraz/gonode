f172dd3adeaf4be43d3febd88878823dd2b1279d
Edison Ferraz <edisonferraz@gmail.com>
2018-12-02 20:09:24 +0100

    Template de e-mail

--- aulas/modulo3/src/app/controllers/PurchaseController.js
+++ aulas/modulo3/src/app/controllers/PurchaseController.js
@@ -13,7 +13,12 @@ class PurchaseController {
       from: '"Edison Ferraz" <edisonferraz@gmail.com>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>Teste---> ${content}</p>`
+      template: 'purchase',
+      context: {
+        user,
+        content,
+        ad: purchaseAd
+      }
     })
 
     return res.send()
