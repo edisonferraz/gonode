616d4432a714debbb2fce80daf8848cf998118a4
Edison Ferraz <edisonferraz@gmail.com>
2018-12-02 19:19:54 +0100

    Filtros

--- aulas/modulo3/src/routes.js
+++ aulas/modulo3/src/routes.js
@@ -4,7 +4,7 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
-const controllers = require('./app/controllers/')
+const controllers = require('./app/controllers')
 
 routes.post('/users', controllers.UserController.store)
 routes.post('/sessions', controllers.SessionController.store)
