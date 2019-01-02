e1651076f6ef054e99fd206ac6563535c83d89dc
Edison <edisonferraz@gmail.com>
2019-01-02 23:06:58 +0100

    Configurando Sentry

--- aulas/modulo4/app/Exceptions/Handler.js
+++ aulas/modulo4/app/Exceptions/Handler.js
@@ -1,8 +1,11 @@
 'use strict'
 
+const Sentry = require('@sentry/node')
+
 const BaseExceptionHandler = use('BaseExceptionHandler')
 const Env = use('Env')
 const Youch = use('youch')
+const Config = use('Config')
 
 /**
  * This class handles all exceptions thrown during
@@ -48,7 +51,10 @@ class ExceptionHandler extends BaseExceptionHandler {
    * @return {void}
    */
   async report (error, { request }) {
-    console.log(error)
+    Sentry.init({
+      dsn: Config.get('services.sentry.dsn')
+    })
+    Sentry.captureException(error)
   }
 }
 
