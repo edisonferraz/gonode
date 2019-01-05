fc9ec08a94c4e7496469bb76606c919effd6b538
Edison <edisonferraz@gmail.com>
2019-01-05 15:53:56 +0100

    criacao, atualizacao de usuario e autenticacao

---
+++ desafios/modulo4/app/Controllers/Http/UserController.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const User = use('App/Models/User')
+const Hash = use('Hash')
+
+class UserController {
+  async store ({ request }) {
+    const data = request.only(['username', 'email', 'password'])
+
+    const user = await User.create(data)
+
+    return user
+  }
+
+  async update ({ request, response, auth }) {
+    const {
+      username,
+      password,
+      newPassword,
+      newPasswordConfirmation
+    } = request.all()
+
+    const verifyNewPasswordMatch = newPassword === newPasswordConfirmation
+    if (!verifyNewPasswordMatch) {
+      return response.status(400).send({
+        error: { message: 'The new password dont match, please retry' }
+      })
+    }
+
+    const user = await auth.getUser()
+    const verifyPassword = await Hash.verify(password, user.password)
+
+    if (!verifyPassword) {
+      return response
+        .status(400)
+        .send({ error: { message: 'This password is wrong, please retry' } })
+    }
+
+    user.username = username
+    user.password = newPassword // await Hash.make(newPassword)
+
+    await user.save()
+
+    return user
+  }
+}
+
+module.exports = UserController
